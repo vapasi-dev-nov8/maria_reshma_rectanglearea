4a4d868cdb26e0c7eaa6115d4c85a7112c6f64f0
rvsathe <reshma30m@gmail.com>
2021-11-12 14:28:36 +0530

    Maria|Reshma changes to make it OOPS-compliant, added code to caluclate perimeter of a rectangle.

--- src/main/java/com/shapes/Rectangle.java
+++ src/main/java/com/shapes/Rectangle.java
@@ -1,6 +1,8 @@
+package com.shapes;
+
 public class Rectangle {
 
     public static int calculate(int width, int height) {
-        return width * height ;
+        return width * height;
     }
 }
