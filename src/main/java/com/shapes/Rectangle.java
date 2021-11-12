523ced01ae387c494e53730d31c2ea315e0938f8
rvsathe <reshma30m@gmail.com>
2021-11-12 14:31:45 +0530

    Maria|Reshma changes to make it OOPS-compliant, added code to calucla

--- src/main/java/com/shapes/Rectangle.java
+++ src/main/java/com/shapes/Rectangle.java
@@ -2,7 +2,35 @@ package com.shapes;
 
 public class Rectangle {
 
-    public static int calculate(int width, int height) {
-        return width * height;
+    private double width;
+    private double breadth;
+
+    public Rectangle(double width, double breadth) {
+        this.width = width;
+        this.breadth = breadth;
+    }
+
+    public void setWidth(Double width){
+        this.width = width;
+    }
+
+    public void setBreadth(Double breadth){
+        this.breadth = breadth;
+    }
+
+    public double getWidth(){
+        return this.width;
+    }
+
+    public double getBreadth(){
+        return this.breadth;
+    }
+
+    public double area() {
+        return getWidth() * getBreadth();
+    }
+
+    public double perimeter() {
+        return 2 * (getBreadth() + getWidth());
     }
 }
--- src/test/java/com/shapes/RectangleClassTest.java
+++ src/test/java/com/shapes/RectangleClassTest.java
@@ -1,11 +1,28 @@
+package com.shapes;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RectangleClassTest {
     @Test
-    public void shouldCalculateAreaWhenWidthandHeightofRectangleGiven()
-    {
-        assertEquals(15,Rectangle.calculate(5,3));
+    public void shouldCalculateAreaWhenWidthandBreadthofRectangleGiven() {
+
+        Rectangle rectangle = new Rectangle(5.0,3.0);
+
+        assertEquals(15.0, rectangle.area());
+
+        Rectangle rectangle1 = new Rectangle(5,3);
+
+        assertEquals(15, rectangle1.area());
+    }
+
+    @Test
+    public void shouldCalculatePerimeterWhenWidthandBreadthofRectangleGiven() {
+
+        Rectangle rectangle = new Rectangle(5.0,3.0);
+
+        assertEquals(16, rectangle.perimeter());
+
     }
 }
